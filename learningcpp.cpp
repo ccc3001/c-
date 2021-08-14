087f61ebaedac46b8de8c5847eae22bd730077cc
ccc3001 <johann.strunck@gmx.de>
2021-08-14 13:19:08 +0200

    class CComplex added but not working

--- learningcpp.cpp
+++ learningcpp.cpp
@@ -15,25 +15,51 @@ public:
 
 class clas
 {
-  std::string names;
 public:
-  void namees();
 
+  void namen();
+  std::string name;
 };
+  void clas::namen()
+ {
+   std::cout<< "halli hallo"<< std::endl;
+ }
 
-void clas::namees(){
-   std::cout<< "halli 1 hallo"<< std::endl;
+/*  class CComplex
+  {
+  public:
+   const int m_re;
+   const int m_im;
+   CComplex(){};
+   explicit CComplex(int a , int b): m_re(a), m_im(b) {};
+   ~String();
+   const int re(){return m_re;};
+   const int im(){return m_im;};
+   auto  add(const Complex &zn)
+   {
+     m_re += zn.re();
+     m_im += zn.im();
+     return *this;
+   };
  };
-
+*/
 
 int main()
 {
+  int b= 10;
+  int *a = &b;
+  int *c = a;
+  int &d = * a ;
+  int *e ;
+  *e= b;
+  std::cout<<b<<" " << *a <<" "<< *c<<" "<< d<<" "<<*e<<std::endl;
   schule an_den_buchen;
   an_den_buchen.anzahl_schueler=20;
   an_den_buchen.name="an_den_buchen";
   an_den_buchen.schnelle_info();
   std::cout<<"helloworld"<<std::endl;
   clas one;
-  one.namees();
+  one.namen();
+
   return 0;
 }
